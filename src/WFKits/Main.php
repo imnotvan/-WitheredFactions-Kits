f7efaf6dac0af66e3bddb257e8ac3362fea144a2
David202020 <31577641+David202020@users.noreply.github.com>
2017-12-10 07:47:49 -0600

    Update Main.php

--- src/WFKits/Main.php
+++ src/WFKits/Main.php
@@ -24,7 +24,7 @@ public function onLoad(){
 public function onJoin(PlayerJoinEvent $event){
    $player = $event->getPlayer();
    $name = $player->getName();
-   $this->getServer()->broadcastMessage(C::GREEN."Ducky Scammed the Server$! He's a dick!")
+   $this->getServer()->broadcastMessage(C::GREEN."Ducky Scammed the Server$! He's a dick!");
 }
 public function onCommand(CommandSender $sender,Command $cmd,string $label,array $args) : bool {
 }
