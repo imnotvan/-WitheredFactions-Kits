f74c17c686b8b293467879ed3553c98677801e72
David202020 <31577641+David202020@users.noreply.github.com>
2017-12-10 07:45:38 -0600

    Update Main.php

--- src/WFKits/Main.php
+++ src/WFKits/Main.php
@@ -9,7 +9,7 @@ use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat as C;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-class Main extends pluginbase implements Listener{
+class Main extends PluginBase implements Listener{
 public function onLoad(){
                     $this->getLogger()->info("Plugin Loading");
           }
@@ -25,14 +25,15 @@ public function onJoin(PlayerJoinEvent $event){
    $player = $event->getPlayer();
    $name = $player->getName();
    $this->getServer()->broadcastMessage(C::GREEN."Ducky Scammed the Server$! He's a dick!")
+}
 public function onCommand(CommandSender $sender,Command $cmd,string $label,array $args) : bool {
 }
 if($cmd->getname() == "wizard"){
  if(!$sender instanceof Player){
           $sender->sendMessage("This Command Only Works for players! Please perform this command IN GAME!");
      }else{
-$sender->sendmessage("Heres Your Wand")
-$sender->getInventory()->addItem(Item::get(280,0,1)
+$sender->sendmessage("Heres Your Wand");
+$sender->getInventory()->addItem(Item::get(280,0,1);
 }
 }
 return true;
